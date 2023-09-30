1c4d9455c8bbe1458da9a377a1d46485d19ebdb7
joze08 <63240949+joze08@users.noreply.github.com>
2023-09-30 16:39:43 -0300

    Criação da Classe D

---
+++ classeD.java
@@ -0,0 +1,12 @@
+public class classeD {
+  private String D1;
+  private int D2;
+
+  public void MD1() {
+    System.out.println("José Airton Rios Junior");
+  }
+
+  public void MD2() {
+    System.out.println("Numero de matricula: 202210444");
+  }
+}
